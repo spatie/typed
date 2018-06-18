dc3667fe3af335536835029bd48a0673bb27d9c1
Freek Van der Herten <freek@spatie.be>
2018-06-18 10:38:18 +0000

    Apply fixes from StyleCI

--- tests/Types/NullTypeTest.php
+++ tests/Types/NullTypeTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Spatie\Typed\Tests\Types;
 
-use PHPUnit\Framework\TestCase;
 use Spatie\Typed\Type;
+use PHPUnit\Framework\TestCase;
 use Spatie\Typed\Types\NullType;
 
 final class NullTypeTest extends TestCase
